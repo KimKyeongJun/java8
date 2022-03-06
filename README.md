13f7a827daac74386ce8672e7ab7e67d2e64ff90
Kyeong Jun <v49011591@gmail.com>
2022-03-06 15:08:15 +0900

    java8 강의 초기 파일 Commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# java8
+# 인프런 백기선님 강의 더 자바, Java 8
+- https://www.inflearn.com/course/the-java-java8/dashboard
---
+++ src/test/java/com/kkj/java8to11/Java8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kkj.java8to11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
